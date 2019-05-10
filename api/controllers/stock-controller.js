a5852d4d7cdf4db1e5590f1eb363ae4c3e08f451
lijy <lijiayaosmail@163.com>
2019-05-10 13:51:48 +0800

    目录更改

--- api/controllers/stock-controller.js
+++ api/controllers/stock-controller.js
@@ -1,12 +1,6 @@
 import stockService from '../services/stock-service.js'
 import {ResultFul} from '../middlewares/result-process';
 import ConstantUtils from '../middlewares/ConstantUtils'
-import mailService from '../services/mail-service'
-import moment from 'moment';
-import path from 'path';
-import fs from 'fs';
-import http from 'http'
-import url from 'url'
 import regeneratorRuntime from '../node_modules/regenerator-runtime/runtime.js';
 
 exports.getStockInfo = async function (req,res) {
